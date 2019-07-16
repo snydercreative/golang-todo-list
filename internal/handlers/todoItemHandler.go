1be643bf96eb2814f6e3757aa7696ded7b347831
Michael Snyder <snydercreative@gmail.com>
2019-07-15 22:25:21 -0400

    Created to handle requests

---
+++ internal/handlers/todoItemHandler.go
@@ -0,0 +1,21 @@
+package handlers
+
+import "net/http"
+
+// TodoItemHandler is a collection of verb handler methods
+type TodoItemHandler struct{}
+
+// Get handles GET requests
+func (t *TodoItemHandler) Get(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Patch handles PATCH requests
+func (t *TodoItemHandler) Patch(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Delete handles DELETE requests
+func (t *TodoItemHandler) Delete(w http.ResponseWriter, r *http.Request) {
+
+}
--- internal/handlers/todoListHandler.go
+++ internal/handlers/todoListHandler.go
@@ -1,5 +1,16 @@
 package handlers
 
-func TodoListHandler() {
+import "net/http"
+
+// TodoListHandler contains methods to deal with HTTP verbs
+type TodoListHandler struct{}
+
+// Get handles the GET requests
+func (t *TodoListHandler) Get(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Post handles the POST requests
+func (t *TodoListHandler) Post(w http.ResponseWriter, r *http.Request) {
 
 }
