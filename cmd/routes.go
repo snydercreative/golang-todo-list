a77d947b5610216052588dd7c0c9101b43211d1b
Michael Snyder <snydercreative@gmail.com>
2019-07-15 20:18:44 -0400

    defining routes and handlers

--- cmd/routes.go
+++ cmd/routes.go
@@ -1,5 +1,21 @@
 package teachingstrategiessample
 
-func Routes() {
+import (
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+// Routes - mappings for the endpoints
+func Routes() {
+	router := mux.NewRouter()
+
+	subrouter := router.PathPrefix("/v1/api").Subrouter()
+
+	subrouter.HandleFunc("/list", TodoListHandler)
+	subrouter.HandleFunc("/list/{listId}", TodoListHandler)
+	subrouter.HandleFunc("/todo/{listId}/todo", TodoItemHandler)
+	subrouter.HandleFunc("/todo/{listId}/todo/{todoId}", TodoItemHandler)
+
+	http.Handle("/", router)
 }
