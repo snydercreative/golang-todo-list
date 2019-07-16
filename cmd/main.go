41f96aa612ea8588538fb81c0ae307c670d4a510
Michael Snyder <snydercreative@gmail.com>
2019-07-15 22:48:20 -0400

    reorg

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,32 @@
 package cmd
 
+import (
+	"golang-todo-list/app/handlers"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	routes()
+	router := mux.NewRouter()
+
+	subrouter := router.PathPrefix("/v1/api").Subrouter()
+
+	todoListsHandler := new(handlers.TodoListsHandler)
+	todoListHandler := new(handlers.TodoListHandler)
+	todoItemHander := new(handlers.TodoItemHandler)
+
+	subrouter.HandleFunc("/lists", todoListsHandler.Get).Methods("GET")
+	subrouter.HandleFunc("/lists", todoListsHandler.Post).Methods("POST")
+	subrouter.HandleFunc("/lists", todoListsHandler.Patch).Methods("PATCH")
+	subrouter.HandleFunc("/lists/{listId}", todoListsHandler.Delete).Methods("DELETE")
+
+	subrouter.HandleFunc("/lists/{listId}/todos", todoListHandler.Get).Methods("GET")
+	subrouter.HandleFunc("/lists/{listId}/todos", todoListHandler.Post).Methods("POST")
+
+	subrouter.HandleFunc("/lists/{listId}/todos/{todoId}", todoItemHander.Get).Methods("GET")
+	subrouter.HandleFunc("/lists/{listId}/todos/{todoId}", todoItemHander.Patch).Methods("PATCH")
+	subrouter.HandleFunc("/lists/{listId}/todos/{todoId}", todoItemHander.Delete).Methods("DELETE")
+
+	http.Handle("/", router)
 }
