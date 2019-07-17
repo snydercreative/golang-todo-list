7b6b7701a29d4a46aaac1d7282e3a06499c8b332
Michael Snyder <snydercreative@gmail.com>
2019-07-16 22:50:50 -0400

    Commented out everything not being used

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,4 @@
-package cmd
+package main
 
 import (
 	"golang-todo-list/app/handlers"
@@ -9,27 +9,30 @@ import (
 )
 
 func main() {
+	repositories.Initialize("postgres://dbuser:sNChv71OhNvILU@localhost/teachingstrategies-sample?sslmode=disable")
+
 	router := mux.NewRouter()
 
 	subrouter := router.PathPrefix("/v1/api").Subrouter()
 
-	todoListsHandler := new(handlers.TodoListsHandler)
+	// todoListsHandler := new(handlers.TodoListsHandler)
 	todoListHandler := new(handlers.TodoListHandler)
-	todoItemHander := new(handlers.TodoItemHandler)
+	// todoItemHander := new(handlers.TodoItemHandler)
 
-	subrouter.HandleFunc("/lists", todoListsHandler.Get).Methods("GET")
-	subrouter.HandleFunc("/lists", todoListsHandler.Post).Methods("POST")
-	subrouter.HandleFunc("/lists", todoListsHandler.Patch).Methods("PATCH")
-	subrouter.HandleFunc("/lists/{listId}", todoListsHandler.Delete).Methods("DELETE")
+	subrouter.HandleFunc("/lists", todoListHandler.Get).Methods("GET")
 
-	subrouter.HandleFunc("/lists/{listId}/todos", todoListHandler.Get).Methods("GET")
-	subrouter.HandleFunc("/lists/{listId}/todos", todoListHandler.Post).Methods("POST")
+	// subrouter.HandleFunc("/lists", todoListsHandler.Post).Methods("POST")
+	// subrouter.HandleFunc("/lists", todoListsHandler.Patch).Methods("PATCH")
+	// subrouter.HandleFunc("/lists/{listId}", todoListsHandler.Delete).Methods("DELETE")
 
-	subrouter.HandleFunc("/lists/{listId}/todos/{todoId}", todoItemHander.Get).Methods("GET")
-	subrouter.HandleFunc("/lists/{listId}/todos/{todoId}", todoItemHander.Patch).Methods("PATCH")
-	subrouter.HandleFunc("/lists/{listId}/todos/{todoId}", todoItemHander.Delete).Methods("DELETE")
+	// subrouter.HandleFunc("/lists/{listId}/todos", todoListHandler.Get).Methods("GET")
+	// subrouter.HandleFunc("/lists/{listId}/todos", todoListHandler.Post).Methods("POST")
+
+	// subrouter.HandleFunc("/lists/{listId}/todos/{todoId}", todoItemHander.Get).Methods("GET")
+	// subrouter.HandleFunc("/lists/{listId}/todos/{todoId}", todoItemHander.Patch).Methods("PATCH")
+	// subrouter.HandleFunc("/lists/{listId}/todos/{todoId}", todoItemHander.Delete).Methods("DELETE")
 
 	http.Handle("/", router)
 
-	repositories.Initialize("postgres://dbuser:sNChv71OhNvILU@localhost/teachingstrategies-sample")
+	http.ListenAndServe(":3000", nil)
 }
